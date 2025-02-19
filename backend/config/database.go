3109eed1fa9536ea3bed8076b414694d1ce2079d
bisre1921 <bisrattewodros3@gmail.com>
2025-02-19 12:29:41 +0300

    setup mongodb connection

---
+++ backend/config/database.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Println("MONGO_URI is not set")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		return err
+	}
+
+	DB = client.Database("agazh")
+	fmt.Println("Connected to MongoDB")
+	return nil
+
+}
