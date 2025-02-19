480d73aad691888f1844eee6fa630868817b6997
bisre1921 <bisrattewodros3@gmail.com>
2025-02-19 19:40:04 +0300

    implement register housekeeper function in auth

---
+++ backend/controllers/auth.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"backend/config"
+	"backend/models"
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func RegisterHouseKeeper(c *gin.Context) {
+	var housekeeper models.Housekeeper
+	if err := c.ShouldBindJSON(&housekeeper); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(housekeeper.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while hashing the password"})
+		return
+	}
+
+	housekeeper.Password = string(hashedPassword)
+	housekeeper.CreatedAt = time.Now()
+	housekeeper.UpdatedAt = time.Now()
+	housekeeper.Rating = 0
+	housekeeper.IsAvailable = true
+
+	result, err := config.DB.Collection("housekeepers").InsertOne(context.Background(), housekeeper)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting housekeeper"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Housekeeper registered successfully", "id": result.InsertedID})
+}
