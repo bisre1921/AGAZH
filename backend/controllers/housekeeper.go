4e6810c9bce060711b6eda4ee47bf64c1273afc1
bisre1921 <bisrattewodros3@gmail.com>
2025-02-20 21:43:43 +0300

    implement API endpoint to fetch a single housekeeper by id

--- backend/controllers/housekeeper.go
+++ backend/controllers/housekeeper.go
@@ -8,10 +8,11 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetHouseKeepers(c *gin.Context) {
+func GetHousekeepers(c *gin.Context) {
 	var filter bson.M = bson.M{}
 
 	if category := c.Query("category"); category != "" {
@@ -42,4 +43,21 @@ func GetHouseKeepers(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, housekeepers)
+}
+
+func GetHousekeeper(c *gin.Context) {
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid housekeeper ID"})
+		return
+	}
+
+	var housekeeper models.Housekeeper
+	err = config.DB.Collection("housekeepers").FindOne(context.Background(), bson.M{"_id": id}).Decode(&housekeeper)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Housekeeper not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, housekeeper)
 }
