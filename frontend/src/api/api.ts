95e1c73a83f3984af72b7c401ad2aa84f47f739c
bisre1921 <bisrattewodros3@gmail.com>
2025-03-01 23:56:44 +0300

    implement api setup with axios, add registration functions for housekeeper and employer, and define data interfaces

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const API_URL = "to be filled";
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+
+api.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+interface HousekeeperData {
+    name: string;
+    email: string;
+    password: string;
+    age: number;
+    experience?: number;
+    category: string; 
+    employmentType: string; 
+    skills?: string[];
+    photoURL?: string;
+    certifications?: string[];
+    location: string;
+    phoneNumber: string;
+    rating?: number;
+    reviews?: string[]; 
+    isAvailable?: boolean;
+}
+
+interface EmployerData {
+    name: string;
+    email: string;
+    password: string;
+    address: string;
+    phoneNumber: string;
+    familySize?: number;
+}
+  
+  
+
+export const registerHousekeeper = (data: HousekeeperData) => {
+    return api.post("/auth/register/housekeeper", data);
+}
+
+export const registerEmployer = (data: EmployerData) => {
+    return api.post("/auth/register/employer", data);
+}
+
+export const login = (email: string, password: string, userType: string) => {
+    return api.post("/auth/login", { email, password, userType });
+}
+
