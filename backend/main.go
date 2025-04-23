1c90d5a3554b759db5c8e1d66339bc559a62e825
bisre1921 <bisrattewodros3@gmail.com>
2025-04-23 17:52:41 +0300

    add vendor folder and update main.go to deploy backend on render

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/config"
 	"backend/routes"
 	"log"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -41,11 +42,15 @@ func main() {
 		routes.SetupRatingRoutes(v1)
 	}
 
-	r.Static("/docs", "./docs") // Serve the docs directory
+	r.Static("/docs", "./docs")
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/docs/swagger.json")))
 
-	if err := r.Run(":8080"); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := r.Run(":" + port); err != nil {
 		log.Fatal("Server Run Failed:", err)
 	}
 }
